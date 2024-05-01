99ae8ea04933a05185300be731e98ddf5249915e
Dourfyt <gamemastertv2016@gmail.com>
2024-05-01 23:21:42 +0300

    New

---
+++ sqlfile.sql
@@ -0,0 +1,86 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.11 (Ubuntu 14.11-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.11 (Ubuntu 14.11-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: tasks; Type: TABLE; Schema: public; Owner: testuser
+--
+
+CREATE TABLE public.tasks (
+    id integer NOT NULL,
+    created_at timestamp(0) without time zone DEFAULT now() NOT NULL,
+    task text
+);
+
+
+ALTER TABLE public.tasks OWNER TO testuser;
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE; Schema: public; Owner: testuser
+--
+
+CREATE SEQUENCE public.tasks_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tasks_id_seq OWNER TO testuser;
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: testuser
+--
+
+ALTER SEQUENCE public.tasks_id_seq OWNED BY public.tasks.id;
+
+
+--
+-- Name: tasks id; Type: DEFAULT; Schema: public; Owner: testuser
+--
+
+ALTER TABLE ONLY public.tasks ALTER COLUMN id SET DEFAULT nextval('public.tasks_id_seq'::regclass);
+
+
+--
+-- Data for Name: tasks; Type: TABLE DATA; Schema: public; Owner: testuser
+--
+
+COPY public.tasks (id, created_at, task) FROM stdin;
+1	2024-05-01 00:20:13	sasd
+2	2024-05-01 00:20:53	fsad
+3	2024-05-01 00:30:47	Поставить чайник
+\.
+
+
+--
+-- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: testuser
+--
+
+SELECT pg_catalog.setval('public.tasks_id_seq', 3, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
